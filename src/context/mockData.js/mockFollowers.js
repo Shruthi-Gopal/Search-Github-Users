492f999285299197ef6b08a0602b755894e05a4e
Shruthi-Gopal <shruthigopal2208@gmail.com>
2021-07-22 11:45:12 +0530

    Uploaded Final

---
+++ src/context/mockData.js/mockFollowers.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    "login": "sxfiy-a",
+    "id": 70191393,
+    "node_id": "MDQ6VXNlcjcwMTkxMzkz",
+    "avatar_url": "https://avatars.githubusercontent.com/u/70191393?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/sxfiy-a",
+    "html_url": "https://github.com/sxfiy-a",
+    "followers_url": "https://api.github.com/users/sxfiy-a/followers",
+    "following_url": "https://api.github.com/users/sxfiy-a/following{/other_user}",
+    "gists_url": "https://api.github.com/users/sxfiy-a/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/sxfiy-a/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/sxfiy-a/subscriptions",
+    "organizations_url": "https://api.github.com/users/sxfiy-a/orgs",
+    "repos_url": "https://api.github.com/users/sxfiy-a/repos",
+    "events_url": "https://api.github.com/users/sxfiy-a/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/sxfiy-a/received_events",
+    "type": "User",
+    "site_admin": false
+  }
+];
